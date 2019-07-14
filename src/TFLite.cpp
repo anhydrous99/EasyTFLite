d6a90159d05e3e1f630bd61075136031b043b054
Armando Herrera <anhydrous99@gmail.com>
2019-07-13 19:29:34 -0500

    Added boost's stacktrace to FATAL glog outputs

--- src/TFLite.cpp
+++ src/TFLite.cpp
@@ -6,19 +6,22 @@
 
 #include <tensorflow/lite/kernels/register.h>
 #include <boost/filesystem.hpp>
+#include <boost/stacktrace.hpp>
 #include <glog/logging.h>
+
+typedef boost::stacktrace::stacktrace stacktrace;
 
 TFLite::TFLite(const boost::filesystem::path &model_path) {
     if (!boost::filesystem::exists(model_path))
-        LOG(FATAL) << "Error: Couldn't find model - " << model_path << '\n';
+        LOG(FATAL) << "Error: Couldn't find model - " << model_path << '\n' << stacktrace();
     if (model_path.extension() != ".tflite")
-        LOG(FATAL) << "Error: model doesn't have .tflite extension\n";
+        LOG(FATAL) << "Error: model doesn't have .tflite extension\n" << stacktrace();
 
     // Build model
     LOG(INFO) << "Building model from file\n";
     model = tflite::FlatBufferModel::BuildFromFile(model_path.string().c_str(), &error_reporter);
     if (model == nullptr)
-        LOG(FATAL) << "Error: Couldn't Build FlatBufferModel from file\n";
+        LOG(FATAL) << "Error: Couldn't Build FlatBufferModel from file\n" << stacktrace();
 
     // Build interpreter
     LOG(INFO) << "Building interpreter from model\n";
@@ -26,37 +29,37 @@ TFLite::TFLite(const boost::filesystem::path &model_path) {
     tflite::InterpreterBuilder builder(*model, resolver);
     auto res = builder(&interpreter);
     if (interpreter == nullptr || res != kTfLiteOk)
-        LOG(FATAL) << "Error: Couldn't Build Interpreter from FlatBufferModel\n";
+        LOG(FATAL) << "Error: Couldn't Build Interpreter from FlatBufferModel\n" << stacktrace();
 
     // Allocate tensor buffers.
     LOG(INFO) << "Allocating tensor buffers\n";
     if (interpreter->AllocateTensors() != kTfLiteOk)
-        LOG(FATAL) << "Couldn't allocate tensor buffers\n";
+        LOG(FATAL) << "Couldn't allocate tensor buffers\n" << stacktrace();
 }
 
 TFLite::TFLite(const boost::filesystem::path &model_path, const tflite::OpResolver &op_resolver) {
     if (!boost::filesystem::exists(model_path))
-        LOG(FATAL) << "Error: Couldn't find model - " << model_path << '\n';
+        LOG(FATAL) << "Error: Couldn't find model - " << model_path << '\n' << stacktrace();
     if (model_path.extension() != ".tflite")
-        LOG(FATAL) << "Error: model doesn't have .tflite extension\n";
+        LOG(FATAL) << "Error: model doesn't have .tflite extension\n" << stacktrace();
 
     // Build model
     LOG(INFO) << "Building model from file\n";
     model = tflite::FlatBufferModel::BuildFromFile(model_path.string().c_str(), &error_reporter);
     if (model == nullptr)
-        LOG(FATAL) << "Error: Couldn't Build FlatBufferModel from file\n";
+        LOG(FATAL) << "Error: Couldn't Build FlatBufferModel from file\n" << stacktrace();
 
     // Build interpreter
     LOG(INFO) << "Building interpreter from model\n";
     tflite::InterpreterBuilder builder(*model, op_resolver);
     auto res = builder(&interpreter);
     if (interpreter == nullptr || res != kTfLiteOk)
-        LOG(FATAL) << "Error: Couldn't Build Interpreter from FlatBufferModel\n";
+        LOG(FATAL) << "Error: Couldn't Build Interpreter from FlatBufferModel\n" << stacktrace();
 
     // Allocate tensor buffers.
     LOG(INFO) << "Allocating tensor buffers\n";
     if (interpreter->AllocateTensors() != kTfLiteOk)
-        LOG(FATAL) << "Couldn't allocate tensor buffers\n";
+        LOG(FATAL) << "Couldn't allocate tensor buffers\n" << stacktrace();
 }
 
 std::vector<int> TFLite::input_tensors() {
