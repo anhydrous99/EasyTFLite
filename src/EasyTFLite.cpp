2875c58a6b6eb7f9a8f0297ce7b6269e4e6c6641
Armando Herrera <anhydrous99@gmail.com>
2019-07-24 19:51:36 -0500

    Switched a couple of functions to template functions to support quantized models in the future

--- src/EasyTFLite.cpp
+++ src/EasyTFLite.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "EasyTFLite.h"
-#include <opencv2/imgproc.hpp>
 
 std::vector<float *> EasyTFLite::run_inference_ptrs(const cv::Mat &image) {
     // Assuming a scale between -1 and 1
@@ -11,75 +10,4 @@ std::vector<float *> EasyTFLite::run_inference_ptrs(const cv::Mat &image) {
         return static_cast<float>(x) / 127.5 - 1;
     };
     return run_inference_ptrs(image, scale_func);
-}
-
-std::vector<float *>
-EasyTFLite::run_inference_ptrs(const cv::Mat &image, const std::function<float(unsigned char)> &scale_func) {
-    std::vector<int> it = input_tensors();
-    std::vector<int> ot = output_tensors();
-    int input_index = it[0];
-
-    // Assuming a one input model
-    if (it.size() != 1)
-        LOG(FATAL) << "Error: OpenCV's Mat inferencing can only be done on models with one input.\n";
-    // Assuming a Rank 4 input tensor for the model with the following [1, x, y, c]
-    std::vector<int> dims = get_tensor_dims(input_index);
-    if (dims.size() != 4)
-        LOG(FATAL) << "Error: OpenCV's Mat inferencing requires a model with a Rank 4 input tensor.\n";
-
-    // Resize image
-    cv::Mat resized_image;
-    cv::Size target_size(dims[1], dims[2]);
-    cv::resize(image, resized_image, target_size);
-
-    // Get the number of elements in image
-    int total_elements = resized_image.channels() * resized_image.rows * resized_image.cols;
-    const unsigned char *resized_image_ptr = resized_image.data;
-
-    // Where converted data will be stored
-    std::vector<float> float_data;
-
-    // Run Scale and convert function
-    std::transform(resized_image_ptr, resized_image_ptr + total_elements, std::back_inserter(float_data),
-                   scale_func);
-
-    // Fill input tensor
-    fill_tensor<float>(float_data.data(), input_index);
-
-    // Invoke model
-    invoke();
-
-    return get_output_tensor_ptrs<float>();
-}
-
-std::vector<float *>
-EasyTFLite::run_inference_ptrs(const cv::Mat &image,
-                               const std::function<std::vector<float>(cv::Mat)> &preprocess_func) {
-    std::vector<int> it = input_tensors();
-    std::vector<int> ot = output_tensors();
-    int input_index = it[0];
-
-    // Assuming a one input model
-    if (it.size() != 1)
-        LOG(FATAL) << "Error: OpenCV's Mat inferencing can only be done on models with one input.\n";
-    // Assuming a Rank 4 input tensor for the model with the following [1, x, y, c]
-    std::vector<int> dims = get_tensor_dims(input_index);
-    if (dims.size() != 4)
-        LOG(FATAL) << "Error: OpenCV's Mat inferencing requires a model with a Rank 4 input tensor.\n";
-
-    // Resize image
-    cv::Mat resized_image;
-    cv::Size target_size(dims[1], dims[2]);
-    cv::resize(image, resized_image, target_size);
-
-    // Apply preprocess function
-    std::vector<float> float_data = preprocess_func(resized_image);
-
-    // Fill input tensor
-    fill_tensor<float>(float_data.data(), input_index);
-
-    // Invoke model
-    invoke();
-
-    return get_output_tensor_ptrs<float>();
 }
--- src/EasyTFLite.h
+++ src/EasyTFLite.h
@@ -9,6 +9,7 @@
 
 #include <functional>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc.hpp>
 
 //! The EasyTFLite class inherits TFLite whose objective is convenience and speed of creating a Tensorflow Lite pipeline
 /*!
@@ -35,28 +36,94 @@ public:
      * to define a custom scale function and preprocess an image between it being scaled and the image data being sent
      * to the interpreter for inference. The model must only have a single input and the input must be a rank 4 Tensor,
      * [1, width, height, channels].
+     * @tparam T The tensor type, must be uint8_t or float, depending if model is quantized or not
      * @param image OpenCV's Mat image to run inference on
      * @param scale_func Your custom scale and preprocesses function, the input of the function must be an unsigned char
      * and the output a float. This function is applied to all elements of image after it has been scaled to the size
      * of model.
      * @return A vector of pointers to the output tensors
      */
-    std::vector<float *>
-    run_inference_ptrs(const cv::Mat &image, const std::function<float(unsigned char)> &scale_func);
+    template<typename T>
+    std::vector<T *> run_inference_ptrs(const cv::Mat &image, const std::function<T(unsigned char)> &scale_func) {
+        std::vector<int> it = input_tensors();
+        std::vector<int> ot = output_tensors();
+        int input_index = it[0];
+
+        // Assuming a one input model
+        if (it.size() != 1)
+            LOG(FATAL) << "Error: OpenCV's Mat inferencing can only be done on models with one input.\n";
+        // Assuming a Rank 4 input tensor for the model with the following [1, x, y, c]
+        std::vector<int> dims = get_tensor_dims(input_index);
+        if (dims.size() != 4)
+            LOG(FATAL) << "Error: OpenCV's Mat inferencing requires a model with a Rank 4 input tensor.\n";
+
+        // Resize image
+        cv::Mat resized_image;
+        cv::Size target_size(dims[1], dims[2]);
+        cv::resize(image, resized_image, target_size);
+
+        // Get the number of elements in image
+        int total_elements = resized_image.channels() * resized_image.rows * resized_image.cols;
+        const unsigned char *resized_image_ptr = resized_image.data;
+
+        // Where converted data will be stored
+        std::vector<T> float_data;
+
+        // Run Scale and convert function
+        std::transform(resized_image_ptr, resized_image_ptr + total_elements, std::back_inserter(float_data),
+                       scale_func);
+
+        // Fill input tensor
+        fill_tensor<T>(float_data.data(), input_index);
+
+        // Invoke model
+        invoke();
+
+        return get_output_tensor_ptrs<T>();
+    }
 
     /*!
      * Runs inference on an OpenCV Mat image, returns a vector of pointers to the output data. You can use this function
      * to define a custom preprocess function between the image being scaled and the image data being sent to the
      * interpreter for inference. The model must only have a single input and the input must be a rank 4 Tensor,
      * [1, width, height, channels].
+     * @tparam T The tensor type, must be uint8_t or float, depending if model is quantized or not
      * @param image OpenCV's Mat image to run inference on
      * @param preprocess_func Your custom preprocess function, the input of the function must be an cv::Mat
      * and the output a vector of floats containing the elements converted to float flattened to a single dimension, C
      * style.
      * @return A vector of pointers to the output tensors
      */
-    std::vector<float *>
-    run_inference_ptrs(const cv::Mat &image, const std::function<std::vector<float>(cv::Mat)> &preprocess_func);
+    template<typename T>
+    std::vector<T *> run_inference_ptrs(const cv::Mat &image, const std::function<std::vector<T>(cv::Mat)> &preprocess_func) {
+        std::vector<int> it = input_tensors();
+        std::vector<int> ot = output_tensors();
+        int input_index = it[0];
+
+        // Assuming a one input model
+        if (it.size() != 1)
+            LOG(FATAL) << "Error: OpenCV's Mat inferencing can only be done on models with one input.\n";
+        // Assuming a Rank 4 input tensor for the model with the following [1, x, y, c]
+        std::vector<int> dims = get_tensor_dims(input_index);
+        if (dims.size() != 4)
+            LOG(FATAL) << "Error: OpenCV's Mat inferencing requires a model with a Rank 4 input tensor.\n";
+
+        // Resize image
+        cv::Mat resized_image;
+        cv::Size target_size(dims[1], dims[2]);
+        cv::resize(image, resized_image, target_size);
+
+        // Apply preprocess function
+        std::vector<T> float_data = preprocess_func(resized_image);
+
+        // Fill input tensor
+        fill_tensor<T>(float_data.data(), input_index);
+
+        // Invoke model
+        invoke();
+
+        return get_output_tensor_ptrs<T>();
+    }
 
     /*!
      * Runs inference where the input is a vector of pointers that point to the flattened input data and the output is
