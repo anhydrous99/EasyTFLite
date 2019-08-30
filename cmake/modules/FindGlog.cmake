54a77f1cd30baa7f36d211fbdfc96267691da7c7
Armando Herrera <anhydrous99@gmail.com>
2019-08-29 19:36:00 -0500

    Small changes to documentation and cmake files to compile without the hunter package manager

---
+++ cmake/modules/FindGlog.cmake
@@ -0,0 +1,54 @@
+# - Try to find Glog
+#
+# The following variables are optionally searched for defaults
+#  GLOG_ROOT_DIR:            Base directory where all GLOG components are found
+#
+# The following are set after configuration is done:
+#  GLOG_FOUND
+#  GLOG_INCLUDE_DIRS
+#  GLOG_LIBRARIES
+#  glog::glog
+
+include(FindPackageHandleStandardArgs)
+
+set(GLOG_ROOT_DIR "" CACHE PATH "Folder contains Google glog")
+
+if (WIN32)
+    find_path(GLOG_INCLUDE_DIR glog/logging.h
+            PATHS ${GLOG_ROOT_DIR}/src/windows)
+else ()
+    find_path(GLOG_INCLUDE_DIR glog/logging.h
+            PATHS ${GLOG_ROOT_DIR})
+endif ()
+
+if (MSVC)
+    find_library(GLOG_LIBRARY_RELEASE libglog_static
+            PATHS ${GLOG_ROOT_DIR}
+            PATH_SUFFIXES Release)
+
+    find_library(GLOG_LIBRARY_DEBUG libglog_static
+            PATHS ${GLOG_ROOT_DIR}
+            PATH_SUFFIXES Debug)
+
+    set(GLOG_LIBRARY optimized ${GLOG_LIBRARY_RELEASE} debug ${GLOG_LIBRARY_DEBUG})
+else ()
+    find_library(GLOG_LIBRARY glog
+            PATHS ${GLOG_ROOT_DIR}
+            PATH_SUFFIXES lib lib64)
+endif ()
+
+find_package_handle_standard_args(Glog DEFAULT_MSG GLOG_INCLUDE_DIR GLOG_LIBRARY)
+
+if (GLOG_FOUND)
+    set(GLOG_INCLUDE_DIRS ${GLOG_INCLUDE_DIR})
+    set(GLOG_LIBRARIES ${GLOG_LIBRARY})
+
+    if (NOT TARGET glog::glog)
+        add_library(glog::glog UNKNOWN IMPORTED)
+        set_target_properties(glog::glog PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${GLOG_INCLUDE_DIRS}")
+        set_property(TARGET glog::glog APPEND PROPERTY IMPORTED_LOCATION "${GLOG_LIBRARY}")
+    endif ()
+
+    mark_as_advanced(GLOG_ROOT_DIR GLOG_LIBRARY_RELEASE GLOG_LIBRARY_DEBUG
+            GLOG_LIBRARY GLOG_INCLUDE_DIR)
+endif ()
